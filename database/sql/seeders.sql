a7fb2cb6340c1ac819ed46d2fb70237142f6333f
Bruno Riwerson Silva <riwerson@gmail.com>
2022-10-28 08:10:58 -0300

    feat: populating database

---
+++ database/sql/seeders.sql
@@ -0,0 +1,26 @@
+TRUNCATE cryptoUpvoting.users;
+INSERT INTO cryptoUpvoting.users (name, email, password)
+VALUES ("Adalberto", "adalberto@email.com", "superSenhaSecreta1"),
+("Giovane", "giovane@email.com", "superSenhaSecreta2"),
+("Jessy", "damasceno@email.com", "superSenhaSecreta3"),
+("Rafael", "rafael@email.com", "superSenhaSecreta4"),
+("Maristela", "maristela@email.com", "superSenhaSecreta5"),
+("Ana", "anahelena@email.com", "superSenhaSecreta6"),
+("Clair", "clair@email.com", "superSenhaSecreta7");
+
+TRUNCATE cryptoUpvoting.cryptos;
+
+INSERT INTO cryptoUpvoting.cryptos (name)
+VALUES ("Bitcoin"), ("Ethereum"), ("Litecoin"),
+("Tether"), ("BNB"), ("USD Coin"),
+("XRP"), ("Binance USD"), ("Cardano"),
+("Solana"), ("Dogecoin"), ("Polygon"),
+("Polkadot"), ("Dai"), ("Shiba Inu");
+
+TRUNCATE cryptoUpvoting.votes;
+INSERT INTO cryptoUpvoting.votes (userId, cryptoId)
+VALUES ("1", "1"), ("1", "2"), ("2", "1"),
+("2", "3"), ("2", "4"), ("3", "2"),
+("4", "4"), ("4", "5"), ("5", "6"),
+("5", "7"), ("6", "8"), ("6", "9"),
+("6", "10"), ("7", "14"), ("7", "15");
