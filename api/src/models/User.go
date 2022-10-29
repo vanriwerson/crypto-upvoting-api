63de08c0f85963681fef60bae6944d09df86ebfd
Bruno Riwerson Silva <riwerson@gmail.com>
2022-10-29 08:29:56 -0300

    feat: create user model

---
+++ api/src/models/User.go
@@ -0,0 +1,56 @@
+package modelos
+
+import (
+	"errors"
+	"strings"
+	"time"
+
+	"github.com/badoux/checkmail"
+)
+
+type User struct {
+	ID        uint64    `json:"id,omitempty"` // omit empty faz com que o campo não seja passado para o JSON caso esteja vazio
+	Name      string    `json:"name,omitempty"`
+	Email     string    `json:"email,omitempty"`
+	Password  string    `json:"password,omitempty"`
+	CreatedAt time.Time `json:"createdAt,omitempty"`
+}
+
+func (user *User) Prepare(stage string) error {
+	if erro := user.validate(stage); erro != nil {
+		return erro
+	}
+
+	if erro := user.format(); erro != nil {
+		return erro
+	}
+
+	return nil
+}
+
+func (user *User) validate(stage string) error {
+	if user.Name == "" {
+		return errors.New("name is required")
+	}
+
+	if user.Email == "" {
+		return errors.New("email is required")
+	}
+
+	if err := checkmail.ValidateFormat(user.Email); err != nil {
+		return errors.New("email must be a valid email")
+	}
+
+	if stage == "creation" && user.Password == "" {
+		return errors.New("password is required")
+	}
+
+	return nil
+}
+
+func (user *User) format() error {
+	user.Name = strings.TrimSpace(user.Name)
+	user.Email = strings.TrimSpace(user.Email)
+
+	return nil
+}
