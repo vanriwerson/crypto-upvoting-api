5c12e50b6b4ba432412dc41a335ecd41abcc5093
Bruno Riwerson Silva <riwerson@gmail.com>
2022-10-29 05:48:56 -0300

    feat: add config package

---
+++ api/src/config/config.go
@@ -0,0 +1,37 @@
+package config
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/joho/godotenv"
+)
+
+var (
+	DbConnectionStr = ""
+	Port            = 0
+)
+
+// LoadEnvVars inicializa as váriaveis de ambiente
+func LoadEnvVars() {
+	var err error
+
+	if err = godotenv.Load(); err != nil {
+		log.Fatal(err)
+	}
+
+	// DbConnectionStr faz a interpolação de user, password, database_name e as opções necessárias para a conexão com o banco de dados
+	DbConnectionStr = fmt.Sprintf("%s:%s@/%s?charset=utf8&parseTime=True&loc=Local",
+		os.Getenv("MYSQL_USER"),
+		os.Getenv("MYSQL_ROOT_PASSWORD"),
+		os.Getenv("MYSQL_DB"),
+	)
+
+	// Port define a porta onde a api estará acessível
+	Port, err = strconv.Atoi(os.Getenv("API_PORT"))
+	if err != nil {
+		Port = 5000
+	}
+}
