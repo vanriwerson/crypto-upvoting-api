12f9b159a336b05c77de0fde5eb7463515d920c4
Bruno Riwerson Silva <riwerson@gmail.com>
2022-10-29 12:52:33 -0300

    feat: calling routes on main package

--- api/main.go
+++ api/main.go
@@ -2,11 +2,17 @@ package main
 
 import (
 	"api/src/config"
+	"api/src/router"
 	"fmt"
+	"log"
+	"net/http"
 )
 
 func main() {
 	config.LoadEnvVars()
 
+	r := router.GenerateRoutes()
+
 	fmt.Printf("Escutando na porta %d", config.Port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", config.Port), r))
 }
