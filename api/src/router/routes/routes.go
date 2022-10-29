dbfb219b9d6a4caec004eb20d686cdda705430c2
Bruno Riwerson Silva <riwerson@gmail.com>
2022-10-29 08:07:10 -0300

    feat: add handler creator for routes

---
+++ api/src/router/routes/routes.go
@@ -0,0 +1,27 @@
+package routes
+
+import (
+	"api/src/middlewares"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type Route struct {
+	URI          string
+	Method       string
+	Controller   func(http.ResponseWriter, *http.Request)
+	RequiresAuth bool
+}
+
+// SetRoutes cria as funções handler para cada rota criada
+func SetRoutes(r *mux.Router) *mux.Router {
+	routes := userRoutes
+	routes = append(routes, loginRoute)
+
+	for _, route := range routes {
+		r.HandleFunc(route.URI, middlewares.Logger(route.Controller)).Methods(route.Method)
+	}
+
+	return r
+}
