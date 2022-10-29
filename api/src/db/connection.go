ac4005c70844c0b6b4f6d7904cc096a5eec0fe3d
Bruno Riwerson Silva <riwerson@gmail.com>
2022-10-29 08:15:39 -0300

    feat: creating dtabase connection package

---
+++ api/src/db/connection.go
@@ -0,0 +1,22 @@
+package db
+
+import (
+	"api/src/config"
+	"database/sql"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func Conect() (*sql.DB, error) {
+	db, err := sql.Open("mysql", config.DbConnectionStr)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = db.Ping(); err != nil {
+		db.Close()
+		return nil, err
+	}
+
+	return db, nil
+}
