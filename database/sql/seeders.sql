82dece0556df48d2c6823284e0057ef13c99cdb0
Bruno Riwerson Silva <riwerson@gmail.com>
2022-10-30 10:20:23 -0300

    chore: adjusting seeders to security features

--- database/sql/seeders.sql
+++ database/sql/seeders.sql
@@ -1,12 +1,13 @@
 TRUNCATE cryptoUpvoting.users;
 INSERT INTO cryptoUpvoting.users (name, email, password)
-VALUES ("Adalberto", "adalberto@email.com", "superSenhaSecreta1"),
-("Giovane", "giovane@email.com", "superSenhaSecreta2"),
-("Jessy", "damasceno@email.com", "superSenhaSecreta3"),
-("Rafael", "rafael@email.com", "superSenhaSecreta4"),
-("Maristela", "maristela@email.com", "superSenhaSecreta5"),
-("Ana", "anahelena@email.com", "superSenhaSecreta6"),
-("Clair", "clair@email.com", "superSenhaSecreta7");
+VALUES ("Adalberto", "adalberto@email.com", "$2a$10$mutnbTgnDCjCxPuU.q1z3.VlEoCtBERyhvw9p.8ML2d49LoaQkWmO"), # password: senha1
+("Giovane", "giovane@email.com", "$2a$10$lYiAF6rla0e6ZeK2Arw34.acjjd28g0A6/2/IDPg5VenpUeoW4Tn2"), # password: senha2
+("Jessy", "jessy@email.com", "$2a$10$9cI08MwoPmcAFkUbCJ54Tusd7uUlHj2Kl6.5MKwV1vhe2NYMeBh.e"), # password: senha3
+("Rafael", "rafael@email.com", "$2a$10$AGRhss/Nv9Ze0KAwoqUHou8RMVqtoX1qDz/JLbuuTX3WxtWL4oMyG"), # password: senha4
+("Maristela", "maristela@email.com", "$2a$10$wpywTh8fB59gwVNqM5IU0OrRNm4IJmJfHIP9kcLrhBoNhepxYFd.q"), # password: senha5
+("Ana", "ana@email.com", "$2a$10$WWfAj/0gO5hx2BrHJmvEMOTnuXxS3Ecjcm/XxJOsdagT3dFW82IuC"), # password: senha6
+("Clair", "clair@email.com", "$2a$10$mZzpwjxuYSUNIRiuDb81.uvsuxMRr.VKdVN5lzm8xnVYCwwgR1tQq"), # password: senha7
+("Admin", "admin@email.com", "$2a$10$kSLHBFOOH4Fiib0E1J1NJO7ABtNrpwT52kutIJy1ojSm1dd69cYyC"); # password: admin
 
 TRUNCATE cryptoUpvoting.cryptos;
 
