91af6beb9ef480d6f2bc3471cc1fad0a3bb57fcb
Bruno Riwerson Silva <riwerson@gmail.com>
2022-10-31 12:31:20 -0300

    fix: adjusting axios baseUrl

--- app/src/services/requestAPI.js
+++ app/src/services/requestAPI.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: `http://localhost:${process.env.REACT_APP_API_PORT || '5000'}`,
+  baseURL: 'http://localhost:5000',
 });
 
 export const setToken = (token) => {
