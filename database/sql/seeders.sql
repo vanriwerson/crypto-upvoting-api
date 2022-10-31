a06e1d78b5ce1bde11e33574c795379d5a71abc1
Bruno Riwerson Silva <riwerson@gmail.com>
2022-10-31 16:25:34 -0300

    fix: deleting unused trucates

--- database/sql/seeders.sql
+++ database/sql/seeders.sql
@@ -1,4 +1,3 @@
-TRUNCATE cryptoUpvoting.users;
 INSERT INTO cryptoUpvoting.users (name, email, password)
 VALUES ("Adalberto", "adalberto@email.com", "$2a$10$mutnbTgnDCjCxPuU.q1z3.VlEoCtBERyhvw9p.8ML2d49LoaQkWmO"), # password: senha1
 ("Giovane", "giovane@email.com", "$2a$10$lYiAF6rla0e6ZeK2Arw34.acjjd28g0A6/2/IDPg5VenpUeoW4Tn2"), # password: senha2
@@ -9,16 +8,12 @@ VALUES ("Adalberto", "adalberto@email.com", "$2a$10$mutnbTgnDCjCxPuU.q1z3.VlEoCt
 ("Clair", "clair@email.com", "$2a$10$mZzpwjxuYSUNIRiuDb81.uvsuxMRr.VKdVN5lzm8xnVYCwwgR1tQq"), # password: senha7
 ("Admin", "admin@email.com", "$2a$10$kSLHBFOOH4Fiib0E1J1NJO7ABtNrpwT52kutIJy1ojSm1dd69cYyC"); # password: admin
 
-TRUNCATE cryptoUpvoting.cryptos;
-
 INSERT INTO cryptoUpvoting.cryptos (name)
 VALUES ("Bitcoin"), ("Ethereum"), ("Litecoin"),
 ("Tether"), ("BNB"), ("USD Coin"),
 ("XRP"), ("Binance USD"), ("Cardano"),
 ("Solana"), ("Dogecoin"), ("Polygon"),
 ("Polkadot"), ("Dai"), ("Shiba Inu");
-
-TRUNCATE cryptoUpvoting.votes;
 INSERT INTO cryptoUpvoting.votes (userId, cryptoId)
 VALUES ("1", "1"), ("1", "2"), ("2", "1"),
 ("2", "3"), ("2", "4"), ("3", "1"), ("3", "2"),
